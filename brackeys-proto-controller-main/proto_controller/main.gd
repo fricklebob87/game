fa825657490f2b0d538b669c333d51a8420b59f5
fricklebob87 <fricklebob5@gmail.com>
2026-01-01 22:30:58 -0600

    a game about a dog

---
+++ brackeys-proto-controller-main/proto_controller/main.gd
@@ -0,0 +1,26 @@
+extends Node3D
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if Global.CreatureState == "staring":
+		$CREATURE2.rotation.y = $ProtoController.rotation.y + 1.5
+	if Global.CreatureState == "riding":
+		$CREATURE2.position.x = $ProtoController.position.x 
+		$CREATURE2.position.z = $ProtoController.position.z
+		$CREATURE2.rotation.y = $ProtoController.rotation.y + -1.5
+	if Global.CreatureState == "moving":
+		if $CREATURE2.position.x <= $ProtoController.position.x:
+			$CREATURE2.position.x = $ProtoController.position.x +3 *delta
+			$CREATURE2.rotation.y = $ProtoController.rotation.y + -1.5 *delta
+			pass
+			#$CREATURE2.rotation.y = $ProtoController.rotation.y + 1.5
+	if $CREATURE2.position.z >= $ProtoController.position.z:
+		$CREATURE2.rotation.y = $ProtoController.rotation.y + -1.5 *delta
+	if $CREATURE2.position == $ProtoController.position:
+		Global.CreatureState = "riding"
+#		Global.CreatureState = "staring"
